9783032e890b654e41a4bdc6cea8285395029324
Balu9911 <sbalu2401@gmail.com>
2024-09-13 18:52:22 +0530

    tst

---
+++ crud.js
@@ -0,0 +1,39 @@
+//create variable
+
+/*let eid=101
+
+//read variable
+console.log(eid)
+
+//update variable
+eid=404
+console.log(eid)
+
+//delete variable
+
+delete eid */
+
+
+/*function get_bal(){
+    console.log("Balance is low")
+}
+get_bal()
+get_bal()
+get_bal() */
+
+/*function low(){
+    console.log("Balance is low")
+}
+while() */
+
+let ei_d=101;
+
+let emp={
+    "id":101,
+    "name":'balu'
+}
+console.log(emp)
+
+let eids=[101,102,103]
+console.log(eids);
+
---
+++ data.js
@@ -0,0 +1,13 @@
+/*let a=10;
+let b=20;
+console.log(a+b); */
+
+/*let eid=101;
+let ename="balu";
+
+function get_details(){}
+
+class Test{} */
+
+
+
