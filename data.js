fd5d908d43a21954b0ffa9abc609addae2e8f73a
Balu9911 <sbalu2401@gmail.com>
2024-09-16 18:45:09 +0530

    class

--- data.js
+++ data.js
@@ -1,13 +1,4 @@
-/*let a=10;
-let b=20;
-console.log(a+b); */
-
-/*let eid=101;
-let ename="balu";
-
-function get_details(){}
-
-class Test{} */
-
-
-
+var a=10;
+var b=20;
+console.log(a+b);
+//document.write(a+b)
---
+++ datatypes/data.js
@@ -0,0 +1,47 @@
+var price=499;
+var rating=3.8;
+var product_Name="mens shirt";
+var avail='y';
+var discount=true;
+var loc;
+//read
+/*console.log(price)
+console.log(discount)
+//using data type  -typeof
+console.log(typeof price)
+console.log(typeof rating) */
+
+/*var product_Name="mens shirt";
+var avail='y'
+var discount=true;
+var loc;
+console.log(product_Name);
+console.log(avail);
+console.log(typeof discount);
+console.log(loc); */
+
+/*var sizes=[32,34,36,38];
+console.log(sizes);
+console.log(typeof sizes); //object */
+
+/*let sizes=[32,34,36,38]
+//group of elments
+let emp={
+    'id':101,
+    'name':"balu"
+}
+console.log(typeof sizes)
+console.log(typeof emp)
+console.log(sizes)
+console.log( emp) */
+
+let emp={
+    id:202,
+    name:"balu"
+}
+let user={
+    email:"rahul&cts.com",
+    email:"balu@ibm.com",
+}
+console.log(emp)
+console.log(user)
