6ce6d0d6efa644198d30effa462d74b5fef20d57
Balu9911 <sbalu2401@gmail.com>
2024-09-17 18:48:23 +0530

    class

---
+++ oper.js
@@ -0,0 +1,58 @@
+let a=10;
+let b=20.5;
+let c="30";
+let d="balu";
+let e="gandhi";
+let f=true;
+let g=false;
+let h;
+/*console.log(a+b);
+console.log(a+c)
+console.log(a+d)
+console.log(a+e)
+console.log(a+f)
+console.log(a+g)
+console.log(a+h)
+console.log(d+e) */
+
+/*console.log(a*b)
+console.log(a*c)
+console.log(a*d)
+console.log(a*e)
+console.log(a*f)
+console.log(a*g)
+console.log(a*h)*/
+
+//let salary=45000;
+//console.log(salary<=40000);
+
+//let z=100;
+//let y="100"
+//console.log(z==y);
+//console.log(z===y);
+
+//let sal=45000;
+//let age=22;
+//console.log(sal>40000 && age>20)//true
+//console.log(sal>46000 && age>20)//false
+//console.log(sal>44000 && age>20)//ture
+//console.log(sal>44000 && age>20)//ture
+//console.log(sal>44000 && age<20)//false
+
+/*let sal=10000
+if(sal>45000){
+    console.log("eligible for marriage");
+}
+else{
+    console.log("not eligible for marriage");
+    
+}*/
+
+let sal=45000;//eligible
+//let sal=5000;//not eligible
+sal>40000?console.log("eligible for marriage"):console.log("not eligible for marriage");
+
+
+
+
+
