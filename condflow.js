237f8e83c38785d7bce34b755cd50cbd8a097b7d
Balu9911 <sbalu2401@gmail.com>
2024-09-18 18:38:23 +0530

    class

---
+++ condflow.js
@@ -0,0 +1,62 @@
+//odd number or even number
+// let num=11;
+// if (num%2==0) {
+//     console.log("Even number");
+// }
+// else{
+//     console.log("Odd number");
+    
+// }
+
+// find odd/even number using ternary operator ?
+// let num=30
+//  num%2?console.log("Even number"):console.log("Odd number");
+
+
+//give num is 3digit num or not
+
+// let num=22;
+// if (num>100 && num<999) {
+//     console.log("yes!,3-digit number");
+// } else {
+//     console.log("No!,not a 3 digit number");
+    
+    
+// }
+
+//given number is divisible by 7 or not
+
+// let num=;
+// if (num%7==0) {
+//     console.log("Yes!,divisible by 7");
+// } else {
+//     console.log("no!,not divisible by 7");
+    
+// }
+
+// let num=70;
+// num%7==0?console.log("Yes!,divisible by 7"):console.log("sorry!,not divisble by 7");   
+
+// let a;
+// let b='balu';
+// // console.log(a);
+// // console.log(b)
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+// if (0.000000000000000009) {
+//     console.log("good");
+    
+    
+// } else {
+//     console.log("bad");
+    
+// }
+
+
+
+
+
+
+
---
+++ iterat.js
@@ -0,0 +1,32 @@
+//1st 10 even numbers
+// for (let i=0;i<=18;i=i+2){
+//     console.log(i);
+    
+// }
+
+
+//print 100 to 51
+// for(let i=100;i>=51;i--){
+//     console.log(i);
+    
+// }
+
+
+
+//print 5table
+
+// for(let i=7;i<=70;i=i+7){
+//     console.log(i);
+    
+// } //o/p:-7,14,21.....70 one way
+
+// for(let i=1;i<=10;i++){
+//     console.log(i*5);    
+// } // 2nd way
+
+for(i=1;i<=10;i++){
+    console.log("5*",i,"=",i*5);
+    
+} //o/p:-5*1=5.....5*10=50.
+
+
