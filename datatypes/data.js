32f484f08543851837daed14f14cdfd3c59e9a08
Balu9911 <sbalu2401@gmail.com>
2024-09-20 18:37:55 +0530

    class

--- datatypes/data.js
+++ datatypes/data.js
@@ -1,9 +1,9 @@
-var price=499;
-var rating=3.8;
-var product_Name="mens shirt";
-var avail='y';
-var discount=true;
-var loc;
+// var price=499;
+// var rating=3.8;
+// var product_Name="mens shirt";
+// var avail='y';
+// var discount=true;
+// var loc;
 //read
 /*console.log(price)
 console.log(discount)
@@ -35,13 +35,71 @@ console.log(typeof emp)
 console.log(sizes)
 console.log( emp) */
 
-let emp={
-    id:202,
-    name:"balu"
-}
-let user={
-    email:"rahul&cts.com",
-    email:"balu@ibm.com",
-}
-console.log(emp)
-console.log(user)
+// let emp={
+//     id:202,
+//     name:"balu"
+// }
+// let user={
+//     email:"rahul&cts.com",
+//     email:"balu@ibm.com",
+// }
+// console.log(emp)
+// console.log(user)
+
+// console.log(emp_id[0]);
+// console.log(emp_id[2]);
+// console.log(emp_id[4]);
+// console.log(emp_id[15]);
+
+// for(i=0;i<=3;i++){
+//     console.log(emp_id[i]);
+// }
+// let emp_id=[101,102,103,104,105]
+// let i=0;
+// while( (i<=5)) {
+//     console.log(emp_id[i]);
+//     i++;
+// }
+
+// for (balu of emp_id) {
+//     console.log(balu);
+// }
+
+// let a={ }
+// let emp={
+//     id:101,
+//     ename:'balu',
+//     loc:'nellore'
+
+ //}
+//     console.log(a);
+//     console.log(emp)
+    // console.log(emp.id)
+    // console.log(emp.ename)
+    // console.log(emp.loc)  
+
+
+// let product={
+//     pname:"marker",
+//     color:['blue','black','red','green'],
+//     price:40,
+//     discount:false
+// }
+// console.log(product);//{......}
+// console.log(product.color)[...]
+// console.log(product.color[3]);//green
+
+// let emp={
+//          id:101,
+//          ename:'balu',
+//          esal:80000
+// }
+// for(prop in emp){
+//     console.log(emp[prop]);
+// } 
+
+
+
+
+
+
--- iterat.js
+++ iterat.js
@@ -24,9 +24,56 @@
 //     console.log(i*5);    
 // } // 2nd way
 
-for(i=1;i<=10;i++){
-    console.log("5*",i,"=",i*5);
+// for(i=1;i<=10;i++){
+//     console.log("5*",i,"=",i*5);
     
-} //o/p:-5*1=5.....5*10=50.
+// } //o/p:-5*1=5.....5*10=50.
+
+//print 1st 5numbers by using for,while,do while...?
+
+//By using for loop
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+    
+// }
+
+// By using while loop
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+    
+// }
+
+//by using dowhile loop
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=5){
+//     console.log(i);  //when we print again in while part it prints upto 6
+    
+// }
+
+//let i=0;
+// for(i=0;i<=20;i=i+2){
+//     console.log(i);
+    
+// }
+
+// while(i<=20){
+//     console.log(i);
+//     i=i+2;
+    
+// }
+
+// do{
+//     console.log(i);
+//     i=i+2;
+// }
+// while(i<=20)
+
 
 
